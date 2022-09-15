0d6ec83e25e59bdc5413c4711085f25c491a418c
Wbam <wbamlaku@gmail.com>
2022-09-15 04:46:52 -0700

    coding c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#inlude<stdio.h> 
+
+/**
+* main-print the string in the input function
+* disription using the main function
+* this program prints "programming is like building a multiligual puzzel"
+* Return 0
+*/
+int main(void)
+ {
+           puts("\"programming  is like building a multilingual puzzel");
+           reatun (0);
+}
