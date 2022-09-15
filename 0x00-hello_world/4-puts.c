f44783f1b4a5181ef43d9684b0c8c4f85830ffee
Wbam <wbamlaku@gmail.com>
2022-09-15 06:08:07 -0700

    coding

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
-#include(stdio.h)
+#include<stdio.h>
 /**
 * main - print the string in the put function
-*
+* 
 * Description: using the main function
-* This programming prints "Programming is like building a multilingual puzzle
-* Return: 0
+* This program prints "Programming is like building a multilingual puzzle
+* Return 0;
 */
 int main(void)
 {
