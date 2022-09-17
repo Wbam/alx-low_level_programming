60728e8f6f62f1eccd3d2cde29fb47d3aceff2ab
Wbam <wbamlaku@gmail.com>
2022-09-17 10:24:31 -0700

    This program will assign a random number to the variable n each time it is executed

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,34 +1,49 @@
 #include <stdlib.h>
+
 #include <time.h>
+
 #include <stdio.h>
-/**
-* main -print positive negative or zerro
-* 
-* Description: using the main function
-* this program prints "positive, negative or zero"
-* Return: 0;
-*/
+
+/** 
+
+* main -prints a random number and state wether it is posetive, negative, or, zero.
+
+* Return: Always 0. */
+
+
+
 int main(void)
-{
-int l;
-int n;
 
-srand(time(0));
-n = ran() -RAND_MAX / 2;
-n =rand() -RAND_MAX / 2;
-l = n % 10;
+{
 
-if (l> 5)
-{
-	 printf("last digit of %d  is  %d and greater than 5\n" ,n ,1);
-}
-else if (1 == 0)
-{
-	printf("last digit of %d is %d and greater than 0\n", n,1);
-}
-else
-{
-	printf("last digit of %d is %d and less than 6 digit and not 0\n", n ,1);
-}
-return (0);
+	int n;
+
+	int l;
+
+
+
+	srand(time(0));
+
+	n = rand() - RAND_MAX / 2;
+
+	l = n % 10
+
+	
+
+	if (l > 5)
+
+		printf("last digit of %d is %d and greater than 5\n", n ,l);
+
+	else if(l == 0)
+
+		printf("last digit of %d is %d and is 0\n", n ,l);
+
+	else
+
+		printf("last digit of %d is %d and is less than 6 and not 0\n", n,l);
+
+
+
+	return (0);
+
 }
