b351357bfddb50e3e7e0ebfc6a355c4bf7f40ab1
Wbam <wbamlaku@gmail.com>
2022-09-17 11:36:28 -0700

    2 print alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+
+* main
+
+*
+
+* Return: Always(success)
+
+*/
+
+int main(void)
+
+{
+
+	char c;
+
+
+
+	for(c ='a'; c<='z',c++)
+
+		putchar(c);
+
+	putchar("\n")
+
+	return(0)
+
+}
