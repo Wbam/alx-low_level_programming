8697ad4c4977933569130f6d8ac86683abb912db
Wbam <wbamlaku@gmail.com>
2022-09-19 10:26:21 -0700

    reset to 98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+#include <stdio.h>
+
+
+
+/**
+
+* reset_to_98 -reset the value of given integer to 98
+
+* @n: parameter
+
+* Return: nothing
+
+*/
+
+void reset_to_98(int *n)
+
+{
+
+	*n=98
+
+}
+
