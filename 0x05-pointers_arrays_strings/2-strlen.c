f8649cb1c40672c57b4a0fdd476709e41316c672
Wbam <wbamlaku@gmail.com>
2022-09-20 09:06:59 -0700

    2-strlen.c

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -2,16 +2,17 @@
 
 /**
 * _strlen - returns the length of a string
-* @s= string
-* Returns= returns length as intiger
+* @s: string
+* Return: lengtha as intiger;
 */
 
 int _strlen(char *s)
 {
 	int len = 0;
-	
+
 	while (*(s + len) != '\0')
 		len++;
 
 	return (len);
 }
+
