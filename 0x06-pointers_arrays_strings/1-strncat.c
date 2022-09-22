077dc9b3602267d5864181e7c9ffcac283a3c7b4
Wbam <wbamlaku@gmail.com>
2022-09-22 11:01:03 -0700

    message

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,46 +1,31 @@
-#include "main.h"                                                                                                                     
+#include " main.h
 
-                                                                                                                                      
+/**
 
-/**                                                                                                                                   
+* _strncat - concatenate two strings but add inputted number of bytes
 
-* _strncat - concatinates two string but add inputed number of bytes                                                                  
+* @dest : string to be appended upon
 
-* @dest: string appended upon                                                                                                         
+* @src : string to be completed at end of dest
 
-* @src: string to be complated at the ende of dest                                                                                    
+* @ n : integer parameter to compare index to
 
-* @n: integer parameter to compare index to                                                                                           
+* Return : returns new concatenated string
 
-* Return: return new concatinated string                                                                                              
+* /
 
-*/                                                                                                                                    
+char * _strncat ( char * dest , char * src , int n )
 
-                                                                                                                                      
+{       int index = 0 , dest_len = 0 ;
 
-char *_strncat(char *dest, char *src, int n)                                                                                          
+        while ( dest [ index ++ ] )
 
-{                                                                                                                                     
+                dest_len ++ ;
 
-        int index =0;                                                                                                                 
+        for ( index = 0 ; src [ index ] && index < n ; index ++ )
 
-        int dest_len =0;                                                                                                              
+                dest [ dest_len ++ ] = src [ index ] ;
 
-                                                                                                                                      
+        return ( dest ) ;
 
-        while (dest[index++])                                                                                                         
-
-                dest_len++;                                                                                                           
-
-                                                                                                                                      
-
-        for (index = 0; src[index] && [index] < n; index++)                                                                           
-
-                dest[dest_len++] = src[index];                                                                                        
-
-                                                                                                                                      
-
-        return (dest);                                                                                                                
-
-}                                                                                                                                     
-
+}
