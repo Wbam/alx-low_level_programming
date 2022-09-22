475d5c52c97134c0d52b8a4bfa278f80e1532e44
Wbam <wbamlaku@gmail.com>
2022-09-22 11:43:22 -0700

    message

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -1,19 +1,37 @@
-#include "main.h"
-/**
-* reverse_array - function that reverses content of array
-* @a: array
-* @n: number of elements
-*/
+#include "main.h"                                                                                                                       
 
-void reverse_array(int *a, int n)
+/**                                                                                                                                     
 
-{
-	int temp;
-	int index;
-	for (index = n - 1; index > n / 2; index--)
-	{
-		temp = a[n - 1 = index];
-		a[n - 1 - index] = a[index];
-		a[index] = temp;
-	}
-}
+* reverse_array - function that reverses content of array                                                                               
+
+* @a: array                                                                                                                             
+
+* @n: number of elements                                                                                                                
+
+*/                                                                                                                                      
+
+                                                                                                                                        
+
+void reverse_array(int *a, int n)                                                                                                       
+
+                                                                                                                                        
+
+{                                                                                                                                       
+
+        int temp;                                                                                                                       
+
+        int index;                                                                                                                      
+
+        for (index = n - 1; index > n / 2; index--)                                                                                     
+
+        {                                                                                                                               
+
+                temp = a[n - 1 = index];                                                                                                
+
+                a[n - 1 - index] = a[index];                                                                                            
+
+                a[index] = temp;                                                                                                        
+
+        }                                                                                                                               
+
+}  
